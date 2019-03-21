949a8b87d31613952e263cbce001daaa93d5344b
StuntmanMel <47351639+StuntmanMel@users.noreply.github.com>
2019-03-21 18:46:54 +0100

    New category: no digit
    Add test data
    Add tester class
    add some comfort functions

--- main.py
+++ main.py
@@ -8,7 +8,16 @@ Created on Mon Mar 18 20:28:13 2019
 
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
+
+def loadimg(name):
+    return (plt.imread(name)).flatten()
+
+def showimg(array):
+    img = np.reshape(array,(28,28))
+    plt.imshow(img)
+
+def randomimg():
+    return np.random.rand(28*28)
 
 def sigmoid(x):
     return 1/(1+np.exp(-x))
@@ -16,31 +25,28 @@ def sigmoid(x):
 def dsigmoid(x):
     return sigmoid(x)*(1-sigmoid(x))
 
-class Network:
-    nc_hl1 = 16
-    nc_hl2 = 16
-    
+class Network:    
     # Input layer
     IL  = np.zeros(28*28)
     # First hidden layer
-    HL1 = np.zeros(nc_hl1)
+    HL1 = np.zeros(32)
     # Second hidden layer
-    HL2 = np.zeros(nc_hl2)
+    HL2 = np.zeros(32)
     # Output layer
-    OL  = np.zeros(10)
+    OL  = np.zeros(11)
     
     # Weight for first hidden layer
-    W1  = (np.random.rand(nc_hl1,28*28))-0.5
+    W1  = (np.random.rand(32,28*28))-0.5
     # Bias for first hidden layer
-    B1  = (np.random.rand(nc_hl1))-0.5
+    B1  = (np.random.rand(32))-0.5
     # Weight for second hidden layer
-    W2  = (np.random.rand(nc_hl2,nc_hl1))-0.5
+    W2  = (np.random.rand(32,32))-0.5
     # Bias for second hidden layer
-    B2  = (np.random.rand(nc_hl2))-0.5
+    B2  = (np.random.rand(32))-0.5
     # Weight for output layer
-    W3  = (np.random.rand(10,nc_hl2))-0.5
+    W3  = (np.random.rand(11,32))-0.5
     # Bias for output layer
-    B3  = (np.random.rand(10))-0.5
+    B3  = (np.random.rand(11))-0.5
 
     # Calculates output layer from input layer
     def calc_network(self):
@@ -66,36 +72,36 @@ class Network:
         self.expected_value = number
     
     def expec_vector(self):
-        vec = np.zeros(10)
+        vec = np.zeros(11)
         vec[self.expected_value] = 1
         return vec
     
-    grad_W3 = (np.zeros((10,16)))
-    grad_B3 = (np.zeros(10))
+    grad_W3 = (np.zeros((11,32)))
+    grad_B3 = (np.zeros(11))
     
     def __backprop_ol(self):
-        for j in range(0,10):
+        for j in range(0,11):
             z = np.dot(self.W3[j],self.HL2) + self.B3[j]
             self.grad_B3[j] = 2*dsigmoid(z)*(self.OL[j]-self.expec_vector()[j])
-            for k in range(0,16):
-                self.grad_W3[j,k] = self.HL2[k] * self.grad_B3[j]
+            
+            self.grad_W3[j,:] = self.HL2 * self.grad_B3[j]
     
-    grad_W2 = np.zeros((16,16))
-    grad_B2 = np.zeros(16)
+    grad_W2 = np.zeros((32,32))
+    grad_B2 = np.zeros(32)
     
     def __backprop_hl2(self):
-        for k in range(0,16):
+        for k in range(0,32):
             prev = np.dot(self.W3[:,k],self.grad_B3)
             z = np.dot(self.W2[k],self.HL1) + self.B2[k]
             self.grad_B2[k] = dsigmoid(z)*prev
-            for i in range(0,16):
-                self.grad_W2[k,i] = self.HL1[i] * self.grad_B2[k]
+            
+            self.grad_W2[k,:] = self.HL1 * self.grad_B2[k]
                 
-    grad_W1 = np.zeros((16,28*28))
-    grad_B1 = np.zeros(16)
+    grad_W1 = np.zeros((32,28*28))
+    grad_B1 = np.zeros(32)
      
     def __backprop_hl1(self):
-        for i in range(0,16):
+        for i in range(0,32):
             prev = np.dot(self.W2[:,i],self.grad_B2)
             z = np.dot(self.W1[i],self.IL) + self.B1[i]
             self.grad_B1[i] = dsigmoid(z)*prev
@@ -111,8 +117,9 @@ class Network:
         self.IL = data
         self.calc_network()
         return np.argmax(self.OL)
-        
-n = Network()
+
+    def feedraw(self,file):
+        self.feed((plt.imread(file)).flatten())
 
 class Trainer:
     data = []
@@ -124,6 +131,14 @@ class Trainer:
                 img = (plt.imread("Training data/"+str(num)+"/"+str(i)+".png")).flatten()
                 self.data.append((num,img))
         
+        for letter in ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]:
+            for i in range(0,200):
+                img = (plt.imread("Training data/letters/"+str(letter)+"/"+str(letter)+"-"+str(i)+".png")).flatten()
+                self.data.append((10,img))
+        
+        for i in range(0,5000):
+            self.data.append((10,randomimg()))
+        
         self.data = np.array(self.data)
         np.random.shuffle(self.data)
     
@@ -132,13 +147,13 @@ class Trainer:
         
     @staticmethod
     def __training_step(network,package):
-        avr_grad_W1 = np.zeros((16,28*28))
-        avr_grad_W2 = np.zeros((16,16))
-        avr_grad_W3 = np.zeros((10,16))
+        avr_grad_W1 = np.zeros((32,28*28))
+        avr_grad_W2 = np.zeros((32,32))
+        avr_grad_W3 = np.zeros((11,32))
         
-        avr_grad_B1 = np.zeros(16)
-        avr_grad_B2 = np.zeros(16)
-        avr_grad_B3 = np.zeros(10)
+        avr_grad_B1 = np.zeros(32)
+        avr_grad_B2 = np.zeros(32)
+        avr_grad_B3 = np.zeros(11)
         
         for (num,data) in package:
             network.set_expected_value(num)
@@ -170,10 +185,51 @@ class Trainer:
             self.__training_step(network,package)
             print ("\r"+str(i)+"/"+str(int(len(self.data)/self.package_size)))
 
-t = Trainer()
-t.load_data()
+class Tester:
+    data = []
+    
+    def load_data(self):
+        for num in [1,2,3,4,5,6,7,8,9,0]:
+            for i in range(0,300):
+                img = (plt.imread("Test data/"+str(num)+"/"+str(5001+i)+".png")).flatten()
+                self.data.append((num,img))
+        
+        for letter in ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]:
+            for i in range(0,200):
+                img = (plt.imread("Test data/letters/"+str(letter)+"/"+str(letter)+"-"+str(201+i)+".png")).flatten()
+                self.data.append((10,img))
+        
+        for i in range(0,200):
+            self.data.append((10,randomimg()))
+        
+        self.data = np.array(self.data)
 
-t.training(n)
+    correct = 0
+    wrong_cases = []
+    
+    def test(self):
+        self.correct = 0
+        self.wrong_cases = []
+        for (num,img) in self.data:
+            erg = n.feed(img)
+            if erg == num:
+                self.correct += 1
+            else:
+                self.wrong_cases.append((erg,num,img))
+        
+        return (self.correct/len(self.data))
 
-def loadimg(name):
-    return (plt.imread(name)).flatten()
+    def show_wrong_case(self,n):
+        return (self.wrong_cases[n][0],self.wrong_cases[n][1],showimg(self.wrong_cases[n][2]))
+
+n = Network()   
+
+tr = Trainer()
+tr.load_data()
+for i in range(0,10):
+    tr.training(n)
+    tr.randomize()
+
+te = Tester()
+te.load_data()
+print(te.test())
